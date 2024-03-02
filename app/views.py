16a772dc515534e1ada630426f81581857c5270c
Rasheeda Francis <rasheedafrancis02@gmail.com>
2024-03-02 10:02:38 -0500

    Exercise 4 Completed

--- app/views.py
+++ app/views.py
@@ -43,9 +43,19 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
-        # Get the username and password values from the form.
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
 
+        # Query the database for the user
+        user = UserProfile.query.filter_by(username=username).first()
+        # Get the username and password values from the form.
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('You have successfully logged in.', 'success')
+            return redirect(url_for('upload'))
+        else:
+            flash('Invalid username or password.', 'error')
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
@@ -53,10 +63,9 @@ def login():
         # passed to the login_user() method below.
 
         # Gets user id, load into session
-        login_user(user)
 
         # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+     # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
