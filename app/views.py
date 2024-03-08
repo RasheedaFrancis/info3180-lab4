0ecf4e232e9edaf28985456f3db81723989e629e
Rasheeda Francis <rasheedafrancis02@gmail.com>
2024-03-08 12:22:33 -0500

    Exercise 5 mostly Completed

--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from app import app, db, login_manager
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
+from werkzeug.security import check_password_hash
 from app.models import UserProfile
 from app.forms import LoginForm
+from app.forms import UploadForm
 
 
 ###
@@ -24,17 +26,21 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
+    
     # Instantiate your form class
-
+    form =UploadForm()
     # Validate file upload on submit
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         # Get file data and save to your uploads folder
-
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File Saved', 'success')
         return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
 
-    return render_template('upload.html')
+    return render_template('upload.html', form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -48,14 +54,15 @@ def login():
         password = form.password.data
 
         # Query the database for the user
-        user = UserProfile.query.filter_by(username=username).first()
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username)).scalar_one()
         # Get the username and password values from the form.
         if user and check_password_hash(user.password, password):
+            
             login_user(user)
             flash('You have successfully logged in.', 'success')
             return redirect(url_for('upload'))
         else:
-            flash('Invalid username or password.', 'error')
+                flash('Invalid username or password.', 'error')
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
