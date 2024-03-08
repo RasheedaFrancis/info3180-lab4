4ab52f842c2bd05faef52076a200c1f69c723621
Rasheeda Francis <rasheedafrancis02@gmail.com>
2024-03-08 14:03:30 -0500

    Exercise 6  Completed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import os
+
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
@@ -18,6 +19,15 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
+
+@app.route('/files')
+@login_required
+def files():
+    images = get_uploaded_images()
+    return render_template('files.html', images=images)
 
 @app.route('/about/')
 def about():
@@ -116,3 +126,11 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+def get_uploaded_images():
+    rootdir = os.getcwd()
+    images = []
+    for subdir, dirs, files in os.walk(rootdir+"/uploads"):
+        for file in files:
+            images.append(file)
+    return images[1:]
